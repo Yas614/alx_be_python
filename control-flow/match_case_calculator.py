8b9fb14f58af7c556b949c2c4d4a6e0153e176cd
Yasmin <yasminaminu111@gmail.com>
2025-09-21 02:55:01 -0700

    Add all control flow tasks

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,21 @@
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+operation = float(input("choose the operation(+, -, * ,/): ")
+match operation:
+case '+':
+result = num1+num2
+print(f"The result is {result}.")
+case "-":
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}.")
+    case _:
+        print("Invalid operation.")
---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather= input("what's the weather like today?(sunny/rainy/cold):")
+if weather =="sunny":
+print("wear t-shirt and sunglasses. ")
+elif weather == "rainy":
+print("don't forger umbrella and raincoat. ")
+elif weather == "cold":
+print("make sure to wear warm coat and scarf. ")
+else:
+print("i don't have any recommendation for this weather. ")
+
